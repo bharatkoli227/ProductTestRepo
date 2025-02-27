75a005b0244a66fa14faebcab5d17befab1177a7
bharat <bharatkoli227@gmail.com>
2025-02-27 17:30:50 +0530

    Add project files.

---
+++ ProductModel/Model/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductModel.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string Image1 { get; set; }
+        public string Image2 { get; set; }
+    }
+}
